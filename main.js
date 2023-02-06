075d8d35520cf2e45d2a19bbb260a67e0f440763
Thomas Lebeau <dev.thomaslebeau@gmail.com>
2023-02-06 10:26:41 +0100

    init

---
+++ main.js
@@ -0,0 +1,14 @@
+const discord = require("discord.js");
+const client = new discord.Client();
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("pong");
+  }
+});
+
+client.login(process.env.TOKEN);
